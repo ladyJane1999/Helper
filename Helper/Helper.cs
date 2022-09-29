60bfe7a4fd594e4d92f6486576e23a473c682a22
ladyJane1999 <anzhela.romanova.99@bk.ru>
2022-09-29 12:54:18 +0300

    Добавьте файлы проекта.

---
+++ Helper/Helper.cs
@@ -0,0 +1,33 @@
+﻿
+public static class Helper
+{
+    public static Task<TResult> WhenAll<TResult>(params Task<TResult>[] task)
+    {
+        TaskCompletionSource<TResult> res = new TaskCompletionSource<TResult>();
+
+        Task.Factory.ContinueWhenAny(new Task<TResult>[] { task[2], task[0], task[1] },
+         t => {
+             foreach (var item in task)
+             {
+                 try
+                 {
+                     if (item.IsFaulted)
+                     {
+                         res.SetException(item.Exception);
+                     }
+                 }
+                 catch (Exception ex)
+                 {
+                     Console.WriteLine(ex.Message);
+                 }
+                 res.SetResult(item.Result);
+             }
+            
+         });
+
+        return res.Task;
+    }
+   
+
+    }
+    
---
+++ Helper/Program.cs
@@ -0,0 +1,33 @@
+﻿
+class Program
+{
+    static async Task Main(string[] args)
+    {
+
+        var tasks = new List<Task<string>>();
+        for (var i = 1; i < 4; i++)
+        {
+            var iteration = i;
+            Task<string> task = Task.Run<string>(async () =>
+            {
+              if(iteration == 2)
+                {
+                    throw new Exception("Exception");
+                }
+                await Task.Delay(100);
+                Console.WriteLine($"Iteration {iteration}");
+                return "iteration";
+            });
+
+            tasks.Add(task);
+        }
+        try
+        {
+            await Helper.WhenAll(tasks[0], tasks[1], tasks[2]);
+        }catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());   
+        }
+      
+    }
+}
